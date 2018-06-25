4baa20c46e1457e85353a6487017fe9bc7e4c973
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-06-25 03:46:13 -0400

    ES6 export syntax.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function getx264Source() {
 
 var x264PathFnc = getx264Source();
 var x264Path = x264PathFnc();
-module.exports = async function(...args) {
+export default async function(...args) {
     try {
         var result = await spawn(x264Path, args, { capture: ["stdout", "stderr"] });
         return result.stdout.toString();
