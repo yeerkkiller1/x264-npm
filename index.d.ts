a4f51bbcfb0b903a40930a1cd9f23307a669361f
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-06-25 03:41:41 -0400

    Added index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module "x264-npm" {
+    /** args are passed directly to the x264 executable, and text from stdout is returned in a promise (anything on stderr causes an error to be thrown, with the text on stderr). */
+    function x264(...args: string[]): Promise<string>;
+    export default x264;
+}
